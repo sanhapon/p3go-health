a8392c9aef3663e392a1a338ff9ef945381e7674
sthadapradit <sthadapradit@smartertravel.com>
2024-09-14 17:24:25 +0700

    edit main page

--- web/src/consts.ts
+++ web/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'p3go.com';
+export const SITE_DESCRIPTION = 'เข้าใจสุขภาพง่าย ๆ ในภาษาที่คุณเข้าใจ p3go.com  นำเสนอข้อมูลสุขภาพที่อ่านง่าย ไม่ซับซ้อน พร้อมเคล็ดลับและคำแนะนำที่นำไปใช้ได้จริง เพื่อสุขภาพที่ดีในทุกวัน';
