5e29981016fba79954d0b974b96189d5a1c526ff
sthadapradit <sthadapradit@smartertravel.com>
2024-09-16 08:54:37 +0700

    fix analytics

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -8,10 +8,12 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://health.p3go.com',
     base: '/',
-    integrations: [mdx(), sitemap(), react(), tailwind()],
+    integrations: [mdx(), sitemap(), react(), tailwind(), partytown()],
 
 });
